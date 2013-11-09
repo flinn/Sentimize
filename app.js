13e5d6224501ee5bfe90d789e99005495a011c19
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-09 14:46:55 -0500

    add charts div

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
+app.set('view cache', false);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
