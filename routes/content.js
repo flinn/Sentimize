960f6368b505000b8e73aa8246a6976e81e208fe
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-09 18:14:04 -0500

    make quotes call

--- routes/content.js
+++ routes/content.js
@@ -1,5 +1,6 @@
-var fool_content = require('../fool.js');
-var sentiments = require('../psychsignal.js');
+var fool_content = require('../fool.js'),
+    get_sentiments = require('../psychsignal.js'),
+    get_quotes = require('../quotes.js');
 
 exports.fool = function(req, res){
   fool_content(req.params.symbol, function(err, contents){
@@ -11,7 +12,7 @@ exports.fool = function(req, res){
 };
 
 exports.sentiments = function(req, res){
-	sentiments(req.params.symbol, req.query.startDate, req.query.endDate, function(err, contents){
+	get_sentiments(req.params.symbol, req.query.startDate, req.query.endDate, function(err, contents){
 
         res.json(contents);
   });
@@ -20,5 +21,8 @@ exports.sentiments = function(req, res){
 
 
 exports.quotes = function(req, res){
+    get_quotes(req.params.symbol, req.query.startDate, req.query.endDate, function(err, contents){
 
+        res.json(contents);
+  });
 };
