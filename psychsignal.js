b2f02e4da02b92c3c9f9e8c3d55696227f238c28
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-09 16:29:02 -0500

    format datte

--- psychsignal.js
+++ psychsignal.js
@@ -1,4 +1,5 @@
 var request = require('request'),
+    moment = require('moment'),
     _  = require('underscore'),
     _s = require('underscore.string');
 
@@ -7,9 +8,34 @@ module.exports = function get_sentiments(symbol, startDate,endDate, fn){
     var url = _s.sprintf("https://psychsignal.com/api/sentiments?api_key=41910b4ec81feab42407b3270cb629d0&symbol=%s&from=%s&to=%s&period=d", symbol, startDate, endDate);
 
     request({url:url, json:true}, function (error, response, contents) {
+
         if (!error && response.statusCode == 200) {
 
-            return fn(null, contents);
+            var data = {
+                "symbol": contents.symbol,
+                "bullish":[],
+                "bearish":[]
+            };
+
+            var bullish = contents.bullish;
+
+            bullish.forEach(function(signal){
+                data.bullish.push({
+                    date: new Date(signal[0]),
+                    value: signal[1]
+                });
+            });
+
+            var bearish = contents.bearish;
+
+            bearish.forEach(function(signal){
+                data.bearish.push({
+                    date: new Date(signal[0]),
+                    value: signal[1]
+                });
+            });
+
+            return fn(null, data);
         }
     });
 };
