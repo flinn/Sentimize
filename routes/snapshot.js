ea23174832c44ee5fd9dcd3812ba8956222f358e
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-09 20:21:42 -0500

    new file structure

--- routes/snapshot.js
+++ routes/snapshot.js
@@ -4,7 +4,6 @@ exports.index = function(req, res){
 
     load_sentiments(req.params.symbol, '2013-07-01', '2013-10-31', function(err, contents){
 
-            console.log(contents);
             res.render('snapshot', {
                 data: contents
             });
