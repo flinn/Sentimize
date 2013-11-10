c89bade76baad463917d060c4e374cd6aed5465b
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-09 23:55:13 -0500

    add fool content to sentimize data

--- routes/content.js
+++ routes/content.js
@@ -3,28 +3,28 @@ var fool_content = require('../fool.js'),
     get_quotes = require('../quotes.js');
 
 exports.fool = function(req, res){
-  fool_content(req.params.symbol, function(err, contents){
+    fool_content(req.params.symbol, function(err, contents){
 
-    res.json(contents);
+        res.json(contents);
 
-  });
+    });
 
 };
 
 exports.sentiments = function(req, res){
 	get_sentiments(req.params.symbol, req.query.startDate, req.query.endDate, function(err, contents){
 
-    res.json(contents);
+        res.json(contents);
 
-  });
+    });
 
 };
 
 exports.quotes = function(req, res){
     get_quotes(req.params.symbol, req.query.startDate, req.query.endDate, function(err, contents){
 
-    res.json(contents);
+        res.json(contents);
 
-  });
+    });
 
 };
