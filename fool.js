45277823d31ff41bb3f34ffe4d2591230339e732
Abhishek Kapatkar <cabhishek@gmail.com>
2013-11-14 13:35:24 -0500

    use api to get instrument ids

--- fool.js
+++ fool.js
@@ -3,16 +3,17 @@ var request = require('request'),
     _  = require('underscore'),
     _s = require('underscore.string');
 
-module.exports = function get_fool_content(symbol, fn){
+module.exports = function get_fool_content(symbol, cb){
 
     async.waterfall([
         function get_instrument(callback){
 
             console.log('Get Instrument for ->' + symbol);
 
-            var url =_s.sprintf("http://apiary.fool.com/Quotes/V3/instruments/usa:%s", symbol);
+            var url =_s.sprintf("http://api.fool.com/quotes/v3/admin/instruments/usa:%s?apikey=J6tv7JMuX6DRTqgWbhFIR8pKEww4YV81", symbol);
 
             request({url:url, json:true}, function (error, response, instrument) {
+                console.log(instrument);
                 if (!error && response.statusCode == 200) {
 
                     callback(null, instrument);
@@ -20,11 +21,11 @@ module.exports = function get_fool_content(symbol, fn){
             });
         },
 
-        function get_content(instrument, callback){
+        function get_foolContent(instrument, callback){
 
             console.log('Got Instrument id ->' + instrument.Id);
 
-            var base_url = "http://apiary.fool.com/Content/Headlines/FindFilteredHeadlinesByInstrumentIds";
+            var base_url = "http://api.fool.com/content/Headlines/FindFilteredHeadlinesByInstrumentIds";
             var params = _s.sprintf("?instrumentIds=%s&startingRow=0&endRow=10&primaryProvider=1&providerLimit=5&contentsiteId=1&providers=1", instrument.Id);
 
             var url = base_url + params;
@@ -39,6 +40,6 @@ module.exports = function get_fool_content(symbol, fn){
     ],
     function(err, results){
 
-        return fn(null, results);
+        return cb(null, results);
     });
 };
